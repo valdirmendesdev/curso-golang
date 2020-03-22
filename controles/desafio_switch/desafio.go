9fbea7f581376edb2eed6ea847950af5edd063de
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2020-03-21 23:10:30 -0300

    Adding switch statement

---
+++ controles/desafio_switch/desafio.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	switch {
+	case n >= 9 && n <= 10:
+		return "A"
+	case n >= 8 && n < 9:
+		return "B"
+	case n >= 5 && n < 8:
+		return "C"
+	case n >= 3 && n < 5:
+		return "D"
+	case n >= 0 && n < 3:
+		return "E"
+	default:
+		return "Nota inválida"
+	}
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(11.1))
+}
